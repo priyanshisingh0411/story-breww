1d857ef36bd8d56a507ba0f3bba22096e3a71fa8
Priyanshi Singh <singhpriyanshi63@gmail.com>
2025-10-19 11:36:40 +0530

    next.config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/story-breww",
+  assetPrefix: "/story-breww",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
